20d9b8db75f859f40c1be5be9ef5222c1b75bf59
Marko Oklobdžija <oklopnik@gmail.com>
2012-11-02 01:26:13 +0100

    Input changes
    Generator now works with layout specifier file and separate layouts. Also, user can specify input and output directories as command-line arguments.

--- generator.py
+++ generator.py
@@ -1,4 +1,6 @@
 def select_menuitem(menu, search_item):
+    """Marks menu item as 'selected' and returns modified menu copy."""
+
     output_menu = list(menu)
 
     for i, item in enumerate(menu):
@@ -12,6 +14,13 @@ def select_menuitem(menu, search_item):
 
 
 def parse_menufile(f):
+    """Menu file parser.
+
+    Parses file based on description line word positions.
+    Marks first and last entry.
+    
+    """
+
     header = f.readline()
 
     name_pos = header.find("Name")
@@ -47,6 +56,8 @@ def parse_menufile(f):
 
 
 def parse_pagefile(f):
+    """Page file parser."""
+
     header = f.readline()
 
     field_pos = header.find("Field")
@@ -73,27 +84,78 @@ def parse_pagefile(f):
     return fields
 
 
+def parse_layouts(f):
+    """Layout file parser."""
+
+    header = f.readline()
+
+    name_pos = header.find("Name")
+    layout_pos = header.find("Layout")
+    head_pos = header.find("Head")
+
+    entries = []
+
+    f.readline() # skip empty line
+
+    # reading entries
+    data = f.readline()
+
+    while data != '':
+        name = data[name_pos:layout_pos].strip()
+        layout = data[layout_pos:head_pos].strip()
+        head = data[head_pos:].strip()
+
+        entry = {}
+
+        entry["Name"] = name
+        entry["Layout"] = layout
+        entry["Head"] = head
+
+        entries.append(entry)
+
+        data = f.readline()
+
+    return entries
+
+
 if __name__ == "__main__":
+    """Template based page generator.
+
+    Arguments:
+    1 - source directory
+    2 - destination directory
+    """
+
     import glob
     import pystache
+    import sys
+    import os
+
+    source_dir = sys.argv[1]
+    dest_dir = sys.argv[2]
+    path_join = os.path.join
+
 
     # parse main menu
-    with open("main.menu", 'r') as main_menu_file:
-        main_menu = parse_menufile(main_menu_file)
+    with open(path_join(source_dir, "main.menu"), 'r') as f:
+        main_menu = parse_menufile(f)
 
     # load main template
-    with open("main.mustache", 'r') as f:
+    with open(path_join(source_dir, "main.mustache"), 'r') as f:
         main_template = f.read()
 
+    # parse content layouts
+    with open(path_join(source_dir, "layouts"), 'r') as f:
+        layouts = parse_layouts(f)
+
     # pass through all pages
-    page_files = glob.glob("*.page")
+    page_files = glob.glob(path_join(source_dir, "*.page"))
+
 
     for filename in page_files:
-        f = open(filename, 'r')
-
-        parsed_data = parse_pagefile(f) # parse page information
-
-        f.close()
+        # parse page information
+        with open(filename, 'r') as f:
+            parsed_data = parse_pagefile(f)
 
         # select main menu entry
         current_main_menu =\
@@ -101,11 +163,9 @@ if __name__ == "__main__":
 
         if ("subm_src" in parsed_data): # if submeny exists
             # parse submenu
-            subm_file = open(parsed_data["subm_src"], 'r')
-
-            current_submenu = parse_menufile(subm_file)
-
-            subm_file.close()
+            with open(path_join(source_dir,\
+                    parsed_data["subm_src"]), 'r') as f:
+                current_submenu = parse_menufile(f)
 
             # select submenu entry
             current_submenu =\
@@ -124,12 +184,27 @@ if __name__ == "__main__":
         if pystache_hash["subm_using"] == True:
             pystache_hash["submenu"] = current_submenu
 
-        # load body content layout file
-        with open(parsed_data["cont_type"], 'r') as cont_file:
-            pystache_hash["body_placeholder"] = cont_file.read()
 
+        # find current layout
+        for layout in layouts:
+            if parsed_data["layout"] == layout["Name"]:
+                # load current layout head and body content
+                if layout["Head"] != "":
+                    with open(path_join(source_dir,\
+                            layout["Head"]), 'r') as f:
+                        pystache_hash["head_layout"] = f.read()
+
+                with open(path_join(source_dir,\
+                        layout["Layout"]), 'r') as f:
+                    pystache_hash["body_layout"] = f.read()
+                
+                break
+
+
+        # load additional head data
         if "head_src" in parsed_data:
-            with open(parsed_data["head_src"], 'r') as head_file:
+            with open(path_join(source_dir, parsed_data["head_src"]),\
+                    'r') as head_file:
                 pystache_hash["head_placeholder"] = head_file.read()
 
         # do first-pass generation
@@ -139,12 +214,13 @@ if __name__ == "__main__":
         pystache_hash = {}
 
         for i, filename in enumerate(parsed_data["cont_src"]):
-            with open(filename, 'r') as cont_file:
+            with open(path_join(source_dir, filename), 'r') as cont_file:
                 pystache_hash["pl" + str(i)] = cont_file.read().strip()
 
         # do second-pass generation
         second_pass = pystache.render(first_pass, pystache_hash)
 
         # write output
-        with open(parsed_data["output"], 'w') as first_pass_file:
-            first_pass_file.write(second_pass)
+        with open(path_join(dest_dir, parsed_data["output"]),\
+                'w') as f:
+            f.write(second_pass)
